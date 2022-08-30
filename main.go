83fc9fbdc30427e311fda6aed59a7c2eec3cc5df
alexanderqbos <alexanderqbos@gmail.com>
2022-08-30 12:17:57 -0700

    Updated examples to use Game dummy data from electron app(Electron-Tournament-App). Disabled Create and Delete routes, they do not apply in the use case of the API

--- main.go
+++ main.go
@@ -2,66 +2,67 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"path"
 
 	"github.com/gorilla/mux"
 )
 
-// Article - Our struct for all articles
-type Article struct {
-	Id      string `json:"Id"`
-	Title   string `json:"Title"`
-	Desc    string `json:"desc"`
-	Content string `json:"content"`
+// Game - Our struct for all Games
+type Game struct {
+	Pool     string `json:"pool"`
+	Team_A   string `json:"team_a"`
+	Team_B   string `json:"team_b"`
+	Time     string `json:"time"`
+	Division string `json:"division"`
 }
 
-var Articles []Article
+var Games []Game
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the HomePage!")
-	fmt.Println("Endpoint Hit: homePage")
+func returnAllGames(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(Games)
 }
 
-func returnAllArticles(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Endpoint Hit: returnAllArticles")
-	json.NewEncoder(w).Encode(Articles)
-}
+func returnGames(w http.ResponseWriter, r *http.Request) {
+	key := path.Base(r.URL.Path)
+	var s []Game
 
-func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
-	vars := r.URL.Query()
-	key := vars.Get("id")
-
-	for _, article := range Articles {
-		if article.Id == key {
-			json.NewEncoder(w).Encode(article)
+	for _, Game := range Games {
+		if Game.Pool == key {
+			s = append(s, Game)
 		}
 	}
+
+	if len(s) > 0 {
+		json.NewEncoder(w).Encode(s)
+	} else {
+		json.NewEncoder(w).Encode("No pools labeled " + key)
+	}
+
 }
 
-func createNewArticle(w http.ResponseWriter, r *http.Request) {
+func createNewGame(w http.ResponseWriter, r *http.Request) {
 	// get the body of our POST request
-	// unmarshal this into a new Article struct
-	// append this to our Articles array.
+	// unmarshal this into a new Game struct
+	// append this to our Games array.
 	reqBody, _ := ioutil.ReadAll(r.Body)
-	var article Article
-	json.Unmarshal(reqBody, &article)
-	// update our global Articles array to include
-	// our new Article
-	Articles = append(Articles, article)
+	var Game Game
+	json.Unmarshal(reqBody, &Game)
+	// update our global Games array to include
+	// our new Game
+	Games = append(Games, Game)
 
-	json.NewEncoder(w).Encode(article)
+	json.NewEncoder(w).Encode(Game)
 }
 
-func deleteArticle(w http.ResponseWriter, r *http.Request) {
-	vars := r.URL.Query()
-	id := vars.Get("id")
+func deleteGame(w http.ResponseWriter, r *http.Request) {
+	id := path.Base(r.URL.Path)
 
-	for index, article := range Articles {
-		if article.Id == id {
-			Articles = append(Articles[:index], Articles[index+1:]...)
+	for index, Game := range Games {
+		if Game.Pool == id {
+			Games = append(Games[:index], Games[index+1:]...)
 		}
 	}
 
@@ -69,18 +70,28 @@ func deleteArticle(w http.ResponseWriter, r *http.Request) {
 
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
-	myRouter.HandleFunc("/", homePage)
-	myRouter.HandleFunc("/articles", returnAllArticles)
-	myRouter.HandleFunc("/article", createNewArticle).Methods("POST")
-	myRouter.HandleFunc("/article/{id}", deleteArticle).Methods("DELETE")
-	myRouter.HandleFunc("/article/{id}", returnSingleArticle)
+	myRouter.HandleFunc("/games", returnAllGames)
+	// myRouter.HandleFunc("/addGame", createNewGame).Methods("POST")
+	// myRouter.HandleFunc("/removeGame/{pool}", deleteGame).Methods("DELETE")
+	myRouter.HandleFunc("/getGame/{pool}", returnGames)
 	log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
 
 func main() {
-	Articles = []Article{
-		Article{Id: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
-		Article{Id: "2", Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+	Games = []Game{
+		{Pool: "A", Team_A: "Swps", Team_B: "Storm", Time: "04 Oct 2022 00:12:00 PST", Division: "Open"},
+		{Pool: "A", Team_A: "Swps", Team_B: "UVIC", Time: "04 Oct 2022 00:12:30 PST", Division: "Open"},
+		{Pool: "A", Team_A: "UVIC", Team_B: "Storm", Time: "04 Oct 2022 00:13:00 PST", Division: "Open"},
+		{Pool: "B", Team_A: "North West", Team_B: "UVIC", Time: "04 Oct 2022 00:12:00 PST", Division: "Bantom"},
+		{Pool: "B", Team_A: "Fraser River", Team_B: "North West", Time: "04 Oct 2022 00:12:30 PST", Division: "Bantom"},
+		{Pool: "B", Team_A: "Swps", Team_B: "North West", Time: "04 Oct 2022 00:13:00 PST", Division: "Bantom"},
+		{Pool: "C", Team_A: "Fraser River", Team_B: "OpenA", Time: "04 Oct 2022 00:12:00 PST", Division: "Atom"},
+		{Pool: "C", Team_A: "AtomA", Team_B: "OpenA", Time: "04 Oct 2022 00:12:30 PST", Division: "Atom"},
+		{Pool: "C", Team_A: "AtomA", Team_B: "Storm", Time: "04 Oct 2022 00:13:00 PST", Division: "Atom"},
+		{Pool: "D", Team_A: "Swps", Team_B: "Storm", Time: "04 Oct 2022 00:12:00 PST", Division: "Peewee"},
+		{Pool: "D", Team_A: "Swps", Team_B: "UVIC", Time: "04 Oct 2022 00:12:30 PST", Division: "Peewee"},
+		{Pool: "D", Team_A: "UVIC", Team_B: "Storm", Time: "04 Oct 2022 00:13:00 PST", Division: "Peewee"},
 	}
+
 	handleRequests()
 }
